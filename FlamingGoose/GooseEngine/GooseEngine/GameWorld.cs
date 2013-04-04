a2c088605ae50c901c9da7363befc2ecc16bfe70
Søren Jacobsen <srnjcbsn@gmail.com>
2013-04-04 15:25:12 +0200

    Minor infrastructure changes

--- FlamingGoose/GooseEngine/GooseEngine/GameWorld.cs
+++ FlamingGoose/GooseEngine/GooseEngine/GameWorld.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using GooseEngine.Entities.MapEntities;
 using GooseEngine.Percepts;
+using GooseEngine.Data;
 
 namespace GooseEngine
 {
--- FlamingGoose/GooseEngine/iilang/iilang/Main.cs
+++ FlamingGoose/GooseEngine/iilang/iilang/Main.cs
@@ -3,52 +3,36 @@ using System.Xml.Linq;
 using System.Xml.Serialization;
 using System.Xml;
 using System.IO;
+using GooseEngine.Data;
 
 namespace iilang
 {
 	class MainClass
 	{
 		public static void Main (string[] args)
-		{
-
-//			Percept f = new Percept (
-//				"navn", 
-//				new Identifier ("hej"),
-//				new ParameterList(
-//					new Numeral(3),
-//			        new Identifier("hello")
-//			    )
-//			);
+		{	
+//			XmlSerializer ser = new XmlSerializer (typeof(EisPercept));
+//			XDocument xwriter = new XDocument ();
+//			XDocument xreader = XDocument.Parse(
+//				@"<percept name=""hej"">
+//					<perceptParameter>
+//						<identifier value=""hejhej"" />
+//					</perceptParameter>
+//					<perceptParameter>
+//						<function name=""fun"">
+//							<number value=""42"" />
+//						</function>
+//					</perceptParameter>
+//				</percept>");
+//			XmlReader r = xreader.CreateReader ();
+//			XmlWriter w = xwriter.CreateWriter ();
 //
-//			XmlSerializer ser = new XmlSerializer(typeof(Percept));
-//			XDocument e = new XDocument ();
-//			XmlWriter w = e.CreateWriter ();
-//			ser.Serialize (w, f);
+//			var par = ser.Deserialize (r);
+//			ser.Serialize (w, par);
+//			
+//			Console.WriteLine (xwriter);
 //
-//			Console.WriteLine (e);
-
-			
-			XmlSerializer ser = new XmlSerializer (typeof(EisPercept));
-			XDocument xwriter = new XDocument ();
-			XDocument xreader = XDocument.Parse(
-				@"<percept name=""hej"">
-					<perceptParameter>
-						<identifier value=""hejhej"" />
-					</perceptParameter>
-					<perceptParameter>
-						<function name=""fun"">
-							<number value=""42"" />
-						</function>
-					</perceptParameter>
-				</percept>");
-			XmlReader r = xreader.CreateReader ();
-			XmlWriter w = xwriter.CreateWriter ();
-
-			var par = ser.Deserialize (r);
-			ser.Serialize (w, par);
-			
-			Console.WriteLine (xwriter);
-
+			Point test = new Vector(1,1) + new Point ();
 
 		}
 	}
