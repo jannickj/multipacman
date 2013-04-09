a87c9fdf343b5a13b33941f0c574de2421f3d3e1
Søren Jacobsen <srnjcbsn@gmail.com>
2013-04-09 17:06:47 +0200

    Java: added new project "GooseEIS", the future GOAL environment. Actual EIS implementation is missing. Contains skeleton code for the Agent class. The XML parser is incomplete and ugly.

---
+++ java goal enviroment/GooseEIS/src/GooseEIS/PerceptReader.java
@@ -0,0 +1,19 @@
+package GooseEIS;
+import eis.iilang.Percept;
+
+
+public class PerceptReader {
+	
+	EisXmlParser parser;
+	
+	public PerceptReader ( EisXmlParser parser)
+	{
+		this.parser = parser;
+	}
+
+	public Percept readPercept() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
