bbe8c6830da296d3b34ecdc7c8b55ee136cffa6f
jannick <jannickboese@msn.com>
2013-04-12 02:10:59 +0200

    Begun work on EISAgentController.cs and fixed ILLParameter to self locate its children

--- FlamingGoose/GooseEngine/GooseEngine/AI/BuiltInController/EISAgentController.cs
+++ FlamingGoose/GooseEngine/GooseEngine/AI/BuiltInController/EISAgentController.cs
@@ -3,23 +3,44 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Xml;
+using System.Xml.Serialization;
 using GooseEngine.Entities.Units;
+using iilang;
 
 namespace GooseEngine.AI.BuiltInController
 {
     public class EISAgentController : AgentController
     {
 
-        public EISAgentController(Agent agent, XmlTextReader xreader) : base(agent)
+        private XmlReader xreader;
+        private XmlWriter xwriter;
+        XmlSerializer deserializer = new XmlSerializer(typeof(IILAction));
+        XmlSerializer serializer = new XmlSerializer(typeof(IILAction));
+
+        public EISAgentController(Agent agent, XmlReader xreader, XmlWriter xwriter) : base(agent)
         {
+            this.xreader = xreader;
+            this.xwriter = xwriter;
 
         }
+
+        private void update()
+        {
+            
+            
+
+        }
+
 
 		#region implemented abstract members of AgentController
 
 		public override void Start ()
 		{
-			throw new NotImplementedException ();
+            while (true)
+            {
+                update();
+            }
+
 		}
 
 		#endregion
--- FlamingGoose/GooseEngine/iilang/iilang/IILParameter.cs
+++ FlamingGoose/GooseEngine/iilang/iilang/IILParameter.cs
@@ -1,22 +1,51 @@
 using System;
 using System.Collections.Generic;
+using System.Reflection;
 using System.Xml.Linq;
+using System.Linq;
+using System.Xml.Serialization;
 
 namespace iilang
 {
 	public abstract class IILParameter : IILElement
 	{
-		private static Dictionary<string,Type> typeMap = new Dictionary<string, Type>() {
-			{"function", typeof(IILFunction)},
-			{"identifier", typeof(IILIdentifier)},
-			{"number", typeof(IILNumeral)},
-			{"parameterList", typeof(IILParameterList)}
-		};
+        private static Dictionary<string, Type> typeMap = new Dictionary<string, Type>();
+        //{
+        //    {"function", typeof(IILFunction)},
+        //    {"identifier", typeof(IILIdentifier)},
+        //    {"number", typeof(IILNumeral)},
+        //    {"parameterList", typeof(IILParameterList)}
+        //};
 
 		public static IILParameter fromString(string str)
 		{
 			return Activator.CreateInstance(typeMap [str]) as IILParameter;
 		}
+
+        static IILParameter()
+        {
+            List<Type> l = FindAllDerivedTypes<IILParameter>();
+            foreach (Type t in l)
+            {
+                XmlRootAttribute att = t.GetCustomAttributes(typeof(XmlRootAttribute), true).FirstOrDefault() as XmlRootAttribute;
+                if (att != null)
+                {
+                    typeMap.Add(att.ElementName, t);
+                }
+            }
+        }
+
+        private static List<Type> FindAllDerivedTypes<T>()
+        {
+            return FindAllDerivedTypes<T>(Assembly.GetAssembly(typeof(T)));
+        }
+
+        private static List<Type> FindAllDerivedTypes<T>(Assembly assembly)
+        {
+            var derivedType = typeof(T);
+            return assembly.GetTypes().Where(t => t != derivedType && derivedType.IsAssignableFrom(t)).ToList();
+
+        }
 	}
 }
 
