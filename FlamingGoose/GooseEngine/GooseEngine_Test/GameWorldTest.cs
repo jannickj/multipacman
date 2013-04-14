45c1b52276a503c992e7ccbe98b1f99577dc2ff1
Søren Jacobsen <srnjcbsn@gmail.com>
2013-04-14 20:53:37 +0200

    Removed redundant vision test

--- FlamingGoose/GooseEngine/GooseEngine_Test/GameWorldTest.cs
+++ FlamingGoose/GooseEngine/GooseEngine_Test/GameWorldTest.cs
@@ -14,35 +14,6 @@ namespace GooseEngine_Test
     [TestFixture]
     public class GameWorldTest
     {
-        
-
-        [Test]
-        public void GetVisibleTiles_WallBlockingNE_ReturnNoVisionNE()
-        {
-
-            GameMap map = new GameMap(new Size(2, 2));
-            Wall SEwall = new Wall();
-            Wall NearWall = new Wall();
-           
-            GameWorld gm = new GameWorld(map);
-
-            gm.AddEntity(new Point(1, 1), NearWall);
-            gm.AddEntity(new Point(2, 2), SEwall);
-
-            Vision v = gm.View(new Point(0,0),2, new Player());
-
-           
-            int expected_count = 0;
-            int actual_count = v.VisibleTiles.Where(p => p.Key.Equals(new Point(2, 2))).Count();
-            Assert.AreEqual(expected_count, actual_count);
-
-            Entity expected = NearWall;
-            Entity actual = v.VisibleTiles.Where(p => p.Key.Equals(new Point(1, 1))).Select(p => p.Value.Entities.First()).First();
-            Assert.AreEqual(expected, actual);
-
-
-        }
-
         [Test]
         public void GetEntityPosition_OneAgentInWorld_ReturnThatAgentPosition()
         {
