2e97d59d1a28d63c37ae4a288493ab04c31a0e08
Søren Jacobsen <srnjcbsn@gmail.com>
2013-04-16 23:06:22 +0200

    C#: implemented the view for agents and walls in consoleview project

--- FlamingGoose/GooseEngine/GooseEngine/GameManagement/Events/UnitMovePostEvent.cs
+++ FlamingGoose/GooseEngine/GooseEngine/GameManagement/Events/UnitMovePostEvent.cs
@@ -2,10 +2,20 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using GooseEngine.Data;
 
 namespace GooseEngine.GameManagement.Events
 {
     public class UnitMovePostEvent : GameEvent
     {
+		private Point newpos;
+		public Point NewPos { 
+			get { return newpos; } 
+		}
+
+		public UnitMovePostEvent(Point newpos)
+		{
+			this.newpos = newpos;
+		}
     }
 }
--- FlamingGoose/GooseEngine/GooseEngineView/Testing/ConsoleView/ConsoleEntityView.cs
+++ FlamingGoose/GooseEngine/GooseEngineView/Testing/ConsoleView/ConsoleEntityView.cs
@@ -3,20 +3,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using GooseEngine;
+using GooseEngine.Data;
+using GooseEngine.Entities;
+using GooseEngine.GameManagement;
+using GooseEngine.GameManagement.Events;
 
 namespace GooseEngineView.Testing.ConsoleView
 {
-    public class ConsoleEntityView
+    public abstract class ConsoleEntityView
     {
-        public ConsoleEntityView(Entity ent)
-        {
+		private Entity model;
+		protected Point position;
 
+		public abstract char Symbol { get; }
+
+		public Entity Model { 
+			get { return model; }
+		}
+
+		public Point Position {
+			get { return position; }
+		}
+
+        public ConsoleEntityView(Entity model)
+        {
+			this.model = model;
+			this.position = model.Position;
+			model.Register(new Trigger<UnitMovePostEvent>( ))
         }
 
-
-
-
-
-        public char Symbol { get; set; }
+		protected virtual void UnitMoved(UnitMovePostEvent evt)
+		{
+			position = evt.NewPos;
+		}
     }
 }
