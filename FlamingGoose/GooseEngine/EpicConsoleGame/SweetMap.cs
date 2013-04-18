8042b03137f4e221e57a1c3c0969d8c52dba6cbb
Søren Jacobsen <srnjcbsn@gmail.com>
2013-04-18 17:59:37 +0200

    C#: VERY basic gamemap

--- FlamingGoose/GooseEngine/EpicConsoleGame/SweetMap.cs
+++ FlamingGoose/GooseEngine/EpicConsoleGame/SweetMap.cs
@@ -4,15 +4,27 @@ using System.Linq;
 using System.Text;
 using GooseEngine;
 using GooseEngine.Data;
+using GooseEngine.Entities.MapEntities;
 
 namespace EpicConsoleGame
 {
     public class SweetMap : GameMap
     {
-        public SweetMap()
-            : base(new Size(6, 6))
+		private List<KeyValuePair<Point,Point>> walls;
+        public SweetMap() : base(new Size(6, 6))
         {
+			BuildMap ();
+			walls  = new List<KeyValuePair<Point, Point>> ()
+			{
+				new KeyValuePair<Point, Point> (new Point (-4, -6), new Point (-4, 5)),
+				new KeyValuePair<Point, Point> (new Point (-2, -4), new Point (-2, 6)),
+			}
+		}
 
-        }
+		private void BuildMap()
+		{
+			foreach (KeyValuePair<Point, Point> kv in walls)
+				this.AddChunk<Wall> (kv.Key, kv.Value);
+		}
     }
 }
