67fc9596d4a005c3aa4317ea2c1684c58397c45f
jannick <jannickboese@msn.com>
2013-04-23 18:35:11 +0200

    Really ugly generic method call fixed

--- FlamingGoose/GooseEngine/GooseEISExtension/EisAgentFactory.cs
+++ FlamingGoose/GooseEngine/GooseEISExtension/EisAgentFactory.cs
@@ -5,12 +5,14 @@ using System.Net;
 using System.Net.Sockets;
 using GooseEISExtension.Controller.AI;
 using GooseEISExtension.Model;
+using GooseEISExtension.Model.Conversion.IILang;
 using GooseEngine;
 using GooseEngine.Conversion;
 using JSLibrary;
 using iilang;
 using GooseEngineController;
 using GooseEngineController.AI;
+using System.Reflection;
 
 namespace GooseEISExtension
 {
@@ -35,9 +37,23 @@ namespace GooseEISExtension
 		{
 			EISConversionTool tool = new EISConversionTool();
 			List<Type> converters = ExtendedType.FindAllDerivedTypes<GooseConverter>();
-			foreach (Type t in converters.Where(t => !t.IsAbstract))
+
+			MethodInfo gmethod = typeof(GooseConversionTool<>).GetMethod("AddConverter");
+
+			foreach (Type t in converters.Where(t => t.BaseType != null && t.BaseType.IsGenericType && !t.IsAbstract))
 			{
-				tool.AddConverter((GooseConverter<GooseObject, IILElement>) Activator.CreateInstance(t));
+				Type actionType = typeof (EISActionConverter<,>);
+				Type perceptType = typeof (EISPerceptConverter<>);
+				Type EISconvert = typeof (EISConverterToEIS<,>);
+
+				Type basetype = t.BaseType.GetGenericTypeDefinition();
+				
+				if (basetype == actionType || basetype == perceptType || basetype == EISconvert)
+				{
+					MethodInfo typedmethod = gmethod.MakeGenericMethod(t.GetGenericArguments());
+					typedmethod.Invoke(tool,new object[]{Activator.CreateInstance(t)});
+				}
+
 			}
 			return tool;
 		}
