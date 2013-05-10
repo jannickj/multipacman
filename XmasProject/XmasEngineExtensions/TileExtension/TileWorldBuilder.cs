ce58e4f382485b80d63a204db17ebcc64d172333
jannick <jannickboese@msn.com>
2013-05-10 14:22:37 +0200

    Tilebuilder

--- XmasProject/XmasEngineExtensions/TileExtension/TileWorldBuilder.cs
+++ XmasProject/XmasEngineExtensions/TileExtension/TileWorldBuilder.cs
@@ -2,10 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using JSLibrary.Data;
 
 namespace XmasEngineExtensions.TileExtension
 {
 	public class TileWorldBuilder
 	{
+		private Size size;
+
+		public TileWorldBuilder(Size size)
+		{
+			this.size = size;
+		}
 	}
 }
