24d67fcf4b7fc5a4a60d11ae0db051b62e2d21f6
jannick johnsen <jannickboese@msn.com>
2013-06-29 04:00:36 +0200

    C#: added a few more comments to the vacuum cleaner example

--- XmasProject/VacuumCleanerWorldExample/Events/VacuumMovedEvent.cs
+++ XmasProject/VacuumCleanerWorldExample/Events/VacuumMovedEvent.cs
@@ -10,7 +10,10 @@ namespace VacuumCleanerWorldExample.Events
 {
 	public class VacuumMovedEvent : XmasEvent
 	{
+        //The position the vacuum moved from
 		public XmasPosition From{get; private set;}
+
+        //The position the vacuum moved to
 		public XmasPosition To { get; private set; }
 
 		public VacuumMovedEvent(XmasPosition from, XmasPosition to)
--- XmasProject/VacuumCleanerWorldExample/VacuumCleanerAgent.cs
+++ XmasProject/VacuumCleanerWorldExample/VacuumCleanerAgent.cs
@@ -13,6 +13,7 @@ namespace VacuumCleanerWorldExample
 		public VacuumCleanerAgent(string name)
 			: base(name)
 		{
+            //Register the Vacuum vision module to the agent, to allow it to recieve percepts
 			this.RegisterModule(new VacuumVisionModule());
 			
 		}
